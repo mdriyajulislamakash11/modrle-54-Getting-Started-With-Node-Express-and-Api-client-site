3fe773cfe9925844ba09f273aeae16bd6df7bdf4
Akash <hafejmohammadakash@gmail.com>
2025-05-15 18:03:17 +0600

    form baniye data nilam

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,45 @@ import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  const [users, setUsers] = useState([])
+  const [users, setUsers] = useState([]);
 
-
-  useEffect(()=> {
+  useEffect(() => {
     fetch("http://localhost:5000/users")
-    .then(res => res.json())
-    .then(data => setUsers(data))
-  }, [])
+      .then((res) => res.json())
+      .then((data) => setUsers(data));
+  }, []);
 
+  const haandleuser = (e) => {
+    e.preventDefault();
+
+    const form = e.target;
+    const name = form.name.value;
+    const email = form.email.value;
+
+    console.log(name, email);
+  };
 
   return (
     <>
       <h1>user management system</h1>
       <p>Numbers Of Users: {users.length}</p>
 
+      {/* user theke information collect */}
+      <form onSubmit={haandleuser}>
+        <input type="text" name="name" />
+        <br />
+        <input type="email" name="email" id="" />
+        <br />
+        <button type="submit">submit</button>
+      </form>
 
-      {
-        users.map((user) => <p key={user.id}>{user.id} {user.name} {user.email} </p>  )
-      }
+      {/*  */}
+
+      {users.map((user) => (
+        <p key={user.id}>
+          {user.id} : {user.name} : {user.email}{" "}
+        </p>
+      ))}
     </>
   );
 }
