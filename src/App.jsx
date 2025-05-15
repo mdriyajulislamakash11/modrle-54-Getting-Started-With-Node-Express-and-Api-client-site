88d5955925c99502eb1da49152e40ff4047633db
Akash <hafejmohammadakash@gmail.com>
2025-05-15 19:06:58 +0600

    data base a pathai ni oita nodemon a er server a coltese nodemon off holei data ar thabe na

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,12 @@ function App() {
       body: JSON.stringify(user)
     })
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => {
+      console.log(data)
+      const newUsers = [...users, data]
+      setUsers(newUsers);
+      form.reset();
+    })
 
   };
 
